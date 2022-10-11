13197a7f9194e5b94f286629d55ab97969cae989
Vladislav <pinevichv@gmail.com>
2022-10-11 12:01:14 +0300

    начал итерационный метод

--- LAB2/src/SOLE.cpp
+++ LAB2/src/SOLE.cpp
@@ -3,3 +3,17 @@
 
 using std::vector;
 
+vector<TT> iteration(const vector< vector<TT>>& matrix, const vector<TT>& vector) {
+	vector< vector<TT>> Ematrix;
+	vector< vector<TT>> result;
+
+	return result;
+}
+
+void getIterational() {
+	vector<vector<TT>> matrix;
+	vector<TT> rightVect;
+	inputMatrix(matrix);
+	inputVector(rightVect);
+	iteration(matrix, rightVect);
+}
