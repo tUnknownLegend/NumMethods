1fee5c07fed0f2de11020ed4e91c7b52b6395f85
Vladislav <pinevichv@gmail.com>
2023-02-22 02:05:27 +0300

    added tests

--- SEM2/LAB1/include/SOLE.h
+++ SEM2/LAB1/include/SOLE.h
@@ -4,9 +4,22 @@
 #include <vector>
 #include "shared.h"
 
-const std::vector<TT> initPoints = {1.0, 0.0};
-const int numOfPoints = 10;
-const std::pair<TT, TT> range = {0, 1};
+ const std::pair<TT, TT> range = {0, 3.5};
+
+// test 1
+// const std::vector<TT> initPoints = {0.0, -0.991};
+ const std::vector<TT> initPoints = {0.0, 0.991};
+
+// test 2
+// const std::vector<TT> initPoints = {0.0, 0.991};
+// const std::vector<TT> initPoints = {0.0, -0.991};
+
+// test 3
+// const std::vector<TT> initPoints = {1.0, 2.0, 3.0};
+
+//const std::vector<TT> initPoints = {1.0, 0.0};
+const int numOfPoints = 100;
+//const std::pair<TT, TT> range = {0.0, 10.0};
 const TT step = TT(range.second - range.first) / TT(numOfPoints);
 
 #endif //INC_LAB_SOLE_H
