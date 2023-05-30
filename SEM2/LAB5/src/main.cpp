28b3ebb1396bca2f51d3ccd7662b4d7d2f5e25a5
Vladislav <pinevichv@gmail.com>
2023-05-30 17:19:50 +0300

    lab 5 ready

--- SEM2/LAB5/src/main.cpp
+++ SEM2/LAB5/src/main.cpp
@@ -2,13 +2,12 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
-#include "shared.h"
 
 std::vector<std::pair<TT, TT>> getRstats(const std::string &path, const std::string &format) {
     std::vector<std::pair<TT, TT>> Rstat;
 
     for (unsigned int i = 10; i <= 1000; i += 10) {
-        Rstat.push_back({i, solve_singular(path + "singular" + format, i)});
+        Rstat.emplace_back(i, solve_singular(path + "singular" + format, i));
     }
     return Rstat;
 }
@@ -18,16 +17,16 @@ int main() {
     const std::string path = "../output/";
     const std::string format = ".dat";
 
-    //Количество разбиений
+    // num of splits
     const size_t N = 100;
 
-//    solve_quadrature(path + "quad" + format, N);
+    solve_quadrature(path + "quad" + format, N);
 
-//    solve_simple_iterations(path + "simple" + format, N, 1e-10, 100);
+    solve_simple_iterations(path + "simple" + format, N, 1e-10, 100);
 
-//    solve_degenerate(path + "degenerate" + format, 80);
+    solve_degenerate(path + "degenerate" + format, 80);
 
-//    solve_singular(path + "singular" + format, N);
+    solve_singular(path + "singular" + format, N);
 
     outputPair(getRstats(path, format), path + "Rstats" + format);
 
